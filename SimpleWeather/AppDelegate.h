747adbead8f0d1dccd96391e69680cb711f13073
devfun <devfun@sina.com>
2015-08-14 12:26:10 +0800

    Initial Commit

---
+++ SimpleWeather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleWeather
+//
+//  Created by 白 云鹏 on 15/8/14.
+//  Copyright (c) 2015年 baiyunpeng.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleWeather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleWeather
+//
+//  Created by 白 云鹏 on 15/8/14.
+//  Copyright (c) 2015年 baiyunpeng.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
